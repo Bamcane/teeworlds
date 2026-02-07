32426cc6e89062d1a59f66f4288bce93d41dcbf0
Bamcane <Bamcane@teemidnight.online>
2026-02-07 22:55:01 +0800

    Display player skin in the chat line

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -11,6 +11,7 @@
 #include <generated/protocol.h>
 #include <generated/client_data.h>
 
+#include <game/client/animstate.h>
 #include <game/client/gameclient.h>
 #include <game/client/localization.h>
 
@@ -640,6 +641,12 @@ void CChat::AddLine(const char *pLine, int ClientID, int Mode, int TargetID)
 		pCurLine->m_Mode = Mode;
 		pCurLine->m_NameColor = -2;
 
+		if(ClientID >= 0)
+		{
+			pCurLine->m_RenderInfo = m_pClient->m_aClients[(Mode == CHAT_WHISPER && ClientID == m_pClient->m_LocalClientID) ? TargetID : ClientID].m_RenderInfo;
+			pCurLine->m_RenderInfo.m_Size = 9.f;
+		}
+
 		// check for highlighted name
 		Highlighted = false;
 		// do not highlight our own messages, whispers and system messages
@@ -1076,13 +1083,17 @@ void CChat::OnRender()
 			{
 				float ClientIDWidth = UI()->GetClientIDRectWidth(FontSize);
 				TextRender()->TextAdvance(&s_ChatCursor, ClientIDWidth);
+				if(Config()->m_ClShowSkinChat)
+					TextRender()->TextAdvance(&s_ChatCursor, FontSize + 1.0f);
 				TextRender()->TextDeferred(&s_ChatCursor, pLine->m_aName, -1);
 				TextRender()->TextDeferred(&s_ChatCursor, ": ", -1);
 			}
 
 			TextRender()->TextDeferred(&s_ChatCursor, pLine->m_aText, -1);
-			pLine->m_Size.y = s_ChatCursor.LineCount() * FontSize;
+			pLine->m_Size.y = s_ChatCursor.LineCount() * FontSize + 1.0f;
 			pLine->m_Size.x = s_ChatCursor.Width();
+			if(pLine->m_RenderInfo.m_aTextures[SKINPART_BODY].IsValid())
+				pLine->m_Size.x += FontSize + 1.0f;
 		}
 	}
 
@@ -1284,6 +1295,14 @@ void CChat::OnRender()
 			TextRender()->TextAdvance(&s_ChatCursor, ClientIDWidth);
 			TextRender()->TextColor(TextColorName);
 			TextRender()->TextSecondaryColor(ShadowColor);
+			if(Config()->m_ClShowSkinChat)
+			{
+				static CAnimState s_State;
+				s_State.Set(&g_pData->m_aAnimations[ANIM_BASE], 1.0f);
+				s_State.Add(&g_pData->m_aAnimations[ANIM_IDLE], 0, 1.0f);
+				RenderTools()->RenderTee(&s_State, &pLine->m_RenderInfo, EMOTE_NORMAL, vec2(1.0f, 0.0f), vec2(s_ChatCursor.AdvancePosition().x + FontSize / 3 * 2 - 1.0f, y + FontSize / 3 * 2 + 1.0f), Blend);
+				TextRender()->TextAdvance(&s_ChatCursor, FontSize + 1.0f);
+			}
 			TextRender()->TextDeferred(&s_ChatCursor, pLine->m_aName, -1);
 			TextRender()->TextDeferred(&s_ChatCursor, ": ", -1);
 			NumNameGlyphs = s_ChatCursor.GlyphCount();
